2a8f3f0f603e10193d6a95e4e49a38f3fd921300
User129863 <118719142+User129863@users.noreply.github.com>
2023-09-24 04:07:44 -0400

    Update __init__.py
    - Added new feature to create Refacet presets.
    - Still buggy, not production ready.

--- __init__.py
+++ __init__.py
@@ -17,6 +17,38 @@ bl_info = {
 handler = SceneHandler()
 plasticity_client = PlasticityClient(handler)
 
+class RefacetPreset(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty(name="Name", default="New Preset")
+    
+    # Basic settings
+    tolerance: bpy.props.FloatProperty(name="Tolerance", default=0.01, min=0.0001, max=0.1, step=0.001, precision=6)                      
+    angle: bpy.props.FloatProperty(name="Angle", default=0.45, min=0.1, max=1.0)
+    
+    # Advanced settings
+    min_width: bpy.props.FloatProperty(name="Min Width", default=0.0, min=0, max=10, unit="LENGTH")    
+    max_width: bpy.props.FloatProperty(name="Max Width", default=0.0, min=0.0001, max=1000.0, step=0.01, soft_min=0.02, precision=6, unit="LENGTH")
+    Edge_chord_tolerance: bpy.props.FloatProperty(name="Edge chord tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
+    Edge_Angle_tolerance: bpy.props.FloatProperty(name="Edge Angle tolerance", default=0.45, min=0.1, max=1.0)
+    Face_plane_tolerance: bpy.props.FloatProperty(name="Face plane tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
+    Face_Angle_tolerance: bpy.props.FloatProperty(name="Face Angle tolerance", default=0.45, min=0.1, max=1.0)     
+
+class AddRefacetPresetOperator(bpy.types.Operator):
+    bl_idname = "refacet_preset.add"
+    bl_label = "Add Refacet Preset"
+    
+    def execute(self, context):
+        preset = context.scene.refacet_presets.add()
+        preset.name = "New Preset"
+        return {'FINISHED'}
+
+class RemoveRefacetPresetOperator(bpy.types.Operator):
+    bl_idname = "refacet_preset.remove"
+    bl_label = "Remove Refacet Preset"
+    
+    def execute(self, context):
+        index = context.scene.active_refacet_preset_index
+        context.scene.refacet_presets.remove(index)
+        return {'FINISHED'}
 
 def select_similar(self, context):
     self.layout.operator(operators.SelectByFaceIDOperator.bl_idname)
@@ -31,6 +63,13 @@ def register():
     bpy.utils.register_class(ui.SubscribeAllButton)
     bpy.utils.register_class(ui.UnsubscribeAllButton)
     bpy.utils.register_class(ui.RefacetButton)
+
+    bpy.utils.register_class(RefacetPreset)
+    bpy.types.Scene.refacet_presets = bpy.props.CollectionProperty(type=RefacetPreset)
+    bpy.types.Scene.active_refacet_preset_index = bpy.props.IntProperty(name="Active Preset", default=0)
+    bpy.utils.register_class(AddRefacetPresetOperator)
+    bpy.utils.register_class(RemoveRefacetPresetOperator) 
+   
     bpy.utils.register_class(ui.PlasticityPanel)
     bpy.utils.register_class(operators.SelectByFaceIDOperator)
     bpy.utils.register_class(operators.SelectByFaceIDEdgeOperator)
@@ -45,14 +84,10 @@ def register():
 
     bpy.types.VIEW3D_MT_edit_mesh_select_similar.append(select_similar)
 
-    bpy.types.Scene.prop_plasticity_server = bpy.props.StringProperty(
-        name="Server", default="localhost:8980")
-    bpy.types.Scene.prop_plasticity_facet_tolerance = bpy.props.FloatProperty(
-        name="Tolerance", default=0.01, min=0.0001, max=0.1, step=0.001, precision=6)
-    bpy.types.Scene.prop_plasticity_facet_angle = bpy.props.FloatProperty(
-        name="Angle", default=0.45, min=0.1, max=1.0)
-    bpy.types.Scene.prop_plasticity_list_only_visible = bpy.props.BoolProperty(
-        name="List only visible", default=False)
+    bpy.types.Scene.prop_plasticity_server = bpy.props.StringProperty(name="Server", default="localhost:8980")
+    bpy.types.Scene.prop_plasticity_facet_tolerance = bpy.props.FloatProperty(name="Tolerance", default=0.01, min=0.0001, max=0.1, step=0.001, precision=6)
+    bpy.types.Scene.prop_plasticity_facet_angle = bpy.props.FloatProperty(name="Angle", default=0.45, min=0.1, max=1.0)
+    bpy.types.Scene.prop_plasticity_list_only_visible = bpy.props.BoolProperty(name="List only visible", default=False)
     bpy.types.Scene.prop_plasticity_facet_tri_or_ngon = bpy.props.EnumProperty(
         items=[
             ("TRI", "Tri", "Tri"),
@@ -61,22 +96,14 @@ def register():
         name="Facet Type",
         default="TRI",
     )
-    bpy.types.Scene.prop_plasticity_ui_show_advanced_facet = bpy.props.BoolProperty(
-        name="Advanced", default=False)
-    bpy.types.Scene.prop_plasticity_facet_min_width = bpy.props.FloatProperty(
-        name="Min Width", default=0.0, min=0, max=10, unit="LENGTH")
-    bpy.types.Scene.prop_plasticity_facet_max_width = bpy.props.FloatProperty(
-        name="Max Width", default=0.0, min=0.0001, max=1000.0, step=0.01, soft_min=0.02, precision=6, unit="LENGTH")
-    bpy.types.Scene.prop_plasticity_unit_scale = bpy.props.FloatProperty(
-        name="Unit Scale", default=1.0, min=0.0001, max=1000.0)
-    bpy.types.Scene.prop_plasticity_curve_chord_tolerance = bpy.props.FloatProperty(
-        name="Edge chord tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
-    bpy.types.Scene.prop_plasticity_curve_angle_tolerance = bpy.props.FloatProperty(
-        name="Edge Angle tolerance", default=0.45, min=0.1, max=1.0)
-    bpy.types.Scene.prop_plasticity_surface_plane_tolerance = bpy.props.FloatProperty(
-        name="Face plane tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
-    bpy.types.Scene.prop_plasticity_surface_angle_tolerance = bpy.props.FloatProperty(
-        name="Face Angle tolerance", default=0.45, min=0.1, max=1.0)
+    bpy.types.Scene.prop_plasticity_ui_show_advanced_facet = bpy.props.BoolProperty(name="Advanced", default=False)
+    bpy.types.Scene.prop_plasticity_facet_min_width = bpy.props.FloatProperty(name="Min Width", default=0.0, min=0, max=10, unit="LENGTH")
+    bpy.types.Scene.prop_plasticity_facet_max_width = bpy.props.FloatProperty(name="Max Width", default=0.0, min=0.0001, max=1000.0, step=0.01, soft_min=0.02, precision=6, unit="LENGTH")
+    bpy.types.Scene.prop_plasticity_unit_scale = bpy.props.FloatProperty(name="Unit Scale", default=1.0, min=0.0001, max=1000.0)
+    bpy.types.Scene.prop_plasticity_curve_chord_tolerance = bpy.props.FloatProperty(name="Edge chord tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
+    bpy.types.Scene.prop_plasticity_curve_angle_tolerance = bpy.props.FloatProperty(name="Edge Angle tolerance", default=0.45, min=0.1, max=1.0)
+    bpy.types.Scene.prop_plasticity_surface_plane_tolerance = bpy.props.FloatProperty(name="Face plane tolerance", default=0.01, min=0.0001, step=0.01, max=1.0, precision=6)
+    bpy.types.Scene.prop_plasticity_surface_angle_tolerance = bpy.props.FloatProperty(name="Face Angle tolerance", default=0.45, min=0.1, max=1.0)
     bpy.types.Scene.mark_seam = bpy.props.BoolProperty(name="Mark Seam")
     bpy.types.Scene.mark_sharp = bpy.props.BoolProperty(name="Mark Sharp")
 
@@ -93,6 +120,13 @@ def unregister():
     bpy.utils.unregister_class(ui.SubscribeAllButton)
     bpy.utils.unregister_class(ui.UnsubscribeAllButton)
     bpy.utils.unregister_class(ui.RefacetButton)
+    
+    bpy.utils.unregister_class(RefacetPreset)
+    bpy.utils.unregister_class(AddRefacetPresetOperator)
+    bpy.utils.unregister_class(RemoveRefacetPresetOperator)
+    del bpy.types.Scene.refacet_presets
+    del bpy.types.Scene.active_refacet_preset_index 
+          
     bpy.utils.unregister_class(operators.SelectByFaceIDOperator)
     bpy.utils.unregister_class(operators.SelectByFaceIDEdgeOperator)
     bpy.utils.unregister_class(operators.AutoMarkEdgesOperator)
